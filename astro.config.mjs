6dd539565343d856b2139a03883c94e03496c411
charly-oryon <charly@yahvehcorporacion.com>
2026-07-06 11:02:59 -0600

    feat: add oryon logo and redict all to /proximamente

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,4 +11,10 @@ export default defineConfig({
   image: {
     domains: ["lh3.googleusercontent.com"],
   },
+  redirects: {
+    "/": {
+      status: 302,
+      destination: "/proximamente",
+    },
+  },
 });
